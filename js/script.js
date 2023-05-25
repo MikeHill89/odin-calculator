885786200d2bf6bb1b582752bf5788906e0c7cbb
MikeHill89 <mvreugdenhil18@gmail.com>
2023-05-25 23:07:12 +0300

    Fix: Reset and Delete not working

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
-//Operator functions
-let n1;
-let n2;
+const display = document.querySelector('.display-digits');
+const digits = document.querySelectorAll('.grid-item');
+const resetButton = document.querySelector('.reset');
+const deleteButton = document.querySelector('.delete');
+const operators = document.querySelectorAll('.operator');
+const calculateButton = document.querySelector('.calculate');
+let tempnumber;
 let operator;
+let n1;
 
-const add = (n1, n2) => n1 + n2;
-const substract = (n1, n2) => n1 - n2;
-const multiply = (n1, n2) => n1 * n2;
-const divide = (n1, n2) => n1 / n2;
+const getNumberFromDisplay = () => {
+    tempnumber = parseInt(display.innerText);
+    return tempnumber;
+}
+const getOperator = (event) => {
+    n1 = tempnumber;
+    tempnumber = 0;
+    operator = event.target.id;
+    return display.innerText = "";
+    //save the tempnumber as n1 get the operator and return it
+}
+const updateDisplay = (event) => {
+    const number = event.target.innerText;
+    display.innerText += number;
+    getNumberFromDisplay();
+}
+digits.forEach((digit) => {
+    digit.addEventListener('click', updateDisplay);
+})
 
-const operate = (operator, n1, n2) => {
+operators.forEach((operator) => {
+    operator.addEventListener('click', getOperator);
+})
+const resetCalc = () => {
+    display.innerText = ""; //reset display
+    tempnumber = 0;
+    n1 = 0;
+}
+const removeDigit = () => {
+    display.innerText = display.innerText.slice(0, -1);
+    getNumberFromDisplay();
+}
+deleteButton.addEventListener('click', removeDigit);
+resetButton.addEventListener('click',resetCalc);
+
+calculateButton.addEventListener('click', function operate() {
     switch (operator) {
-        case "add":
-            return add(n1, n2);
-        case "substract":
-            return substract(n1, n2);
         case "multiply":
-            return multiply(n1, n2);
-        case "divide":
-            return divide(n1, n2);
+            return display.innerText = n1 * tempnumber;
         default:
-            break;
+            return display.innerText = "operator not recognized";
     }
-};
+});
