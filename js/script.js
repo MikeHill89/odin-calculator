75673ed9145d9deaf549a08ffebecb8fb7b7edd8
MikeHill89 <mvreugdenhil18@gmail.com>
2023-06-05 21:12:19 +0300

    Fix: Display updating

--- js/script.js
+++ js/script.js
@@ -1,53 +1,83 @@
-const display = document.querySelector('.display-digits');
-const digits = document.querySelectorAll('.grid-item');
+//DOM elements
+const display = document.querySelector('.current');
+const previous = document.querySelector('.previous');
+const numbers = document.querySelectorAll('.number');
 const resetButton = document.querySelector('.reset');
 const deleteButton = document.querySelector('.delete');
 const operators = document.querySelectorAll('.operator');
-const calculateButton = document.querySelector('.calculate');
-let tempnumber;
-let operator;
-let n1;
+const calculateButton = document.querySelector('#calculate');
 
-const getNumberFromDisplay = () => {
-    tempnumber = parseFloat(display.innerText);
-    return tempnumber;
-}
-const getOperator = (event) => {
-    n1 = tempnumber;
-    tempnumber = 0;
-    operator = event.target.id;
-    return display.innerText = "";
-    //save the tempnumber as n1 get the operator and return it
-}
-const updateDisplay = (event) => {
-    const number = event.target.innerText;
-    display.innerText += number;
-    getNumberFromDisplay();
-}
-digits.forEach((digit) => {
-    digit.addEventListener('click', updateDisplay);
+let currentValue = 0;
+let savedValue = 0;
+let operatorUsed;
+let result;
+
+resetButton.addEventListener('click', () => {
+    currentValue = 0;
+    savedValue = 0;
+    operatorUsed = "";
+    result = "";
+    display.textContent ="";
+    previous.textContent = "";
+    console.log('reset everything');
+});
+
+deleteButton.addEventListener('click', () => {
+    console.log('deleted last number or operator');
+    const deleteLastNumber = display.textContent.slice(0, -1);
+    display.textContent = deleteLastNumber;
+    currentValue = parseFloat(display.textContent);
+    
+});
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (event) => {
+        let clickedNumber = event.currentTarget.dataset.value;
+        display.textContent += clickedNumber;
+        currentValue = parseFloat(display.textContent);
+        console.log(parseFloat(clickedNumber));
+    })
 })
 
 operators.forEach((operator) => {
-    operator.addEventListener('click', getOperator);
+    operator.addEventListener('click', (event) => {
+        savedValue = currentValue;
+        currentValue = 0;
+        let clickedOperator = event.currentTarget;
+        operatorUsed = clickedOperator.textContent;
+        console.log(operatorUsed);
+        display.textContent = "";
+        previous.textContent = `${savedValue + operatorUsed} `;
+    });
 })
-const resetCalc = () => {
-    display.innerText = ""; //reset display
-    tempnumber = 0;
-    n1 = 0;
-}
-const removeDigit = () => {
-    display.innerText = display.innerText.slice(0, -1);
-    getNumberFromDisplay();
-}
-deleteButton.addEventListener('click', removeDigit);
-resetButton.addEventListener('click',resetCalc);
-
-calculateButton.addEventListener('click', function operate() {
+const operate = (operator, n1, n2) => {
+    console.log("Checking Operands and Operator");
+    console.log(`Saved value = ${savedValue}`)
+    console.log(`Operator used = ${operatorUsed}`)
+    console.log(`Current value = ${currentValue}`);
+    let result;
     switch (operator) {
-        case "multiply":
-            return display.innerText = n1 * tempnumber;
+        case "+":
+            result = n1 + n2;
+            break;
+        case "-":
+            result = n1 - n2;
+            break;
+        case "X":
+            result = n1 * n2;
+            break;
+        case "÷":
+            result = n1 / n2;
+            break;
         default:
-            return display.innerText = "operator not recognized";
+            return n2; // Return the second value if operator is not supported
     }
+    console.log(`The result is ${result}`);
+    previous.textContent += `${currentValue}`;
+    currentValue = result;
+    return result;
+};
+
+calculateButton.addEventListener('click', () =>{ 
+    display.textContent = operate(operatorUsed,savedValue, currentValue);
 });
